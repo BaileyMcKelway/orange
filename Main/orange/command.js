3c5ea803be92452b8bfa37330b4d1b7c17434101
Bailey McKelway <bjlmckelway@gmail.com>
2020-05-25 13:14:34 -0400

    remove button path start

--- Main/orange/command.js
+++ Main/orange/command.js
@@ -9,39 +9,41 @@ chrome.extension.onMessage.addListener(function (
 
   const hostKeys = Object.keys(recordedCommands);
   console.log(recordedCommands[hostKeys]);
-  //   hostKeys.forEach((key) => {
-  //     console.log(recordedCommands[key]);
-  //   });
-  //   const innerKeys = Object.keys(recordedCommands[hostKeys]);
-  const div = document.createElement('div');
+  if (hostKeys.length === 0) {
+    const commands = document.querySelector('#commands');
+    const p = document.createElement('p');
+    p.textContent = 'No recorded commands';
+    commands.append(p);
+  } else {
+    const div = document.createElement('div');
 
-  hostKeys.forEach((mainKeys) => {
-    const table = document.createElement('table');
-    const tr = document.createElement('tr');
-    const th = document.createElement('th');
-    th.textContent = mainKeys;
-    tr.appendChild(th);
-    const mainKeyObject = recordedCommands[mainKeys];
-    const secondaryKeys = Object.keys(mainKeyObject);
+    hostKeys.forEach((mainKeys) => {
+      const table = document.createElement('table');
+      const tr = document.createElement('tr');
+      const th = document.createElement('th');
+      th.textContent = mainKeys;
+      tr.appendChild(th);
+      const mainKeyObject = recordedCommands[mainKeys];
+      const secondaryKeys = Object.keys(mainKeyObject);
 
-    table.appendChild(tr);
-    secondaryKeys.forEach((secondary) => {
-      const secondtr = document.createElement('tr');
-      const td = document.createElement('td');
-      const a = document.createElement('a');
-      a.classList.add('square_btn35');
-      const linkText = document.createTextNode('x');
-      a.appendChild(linkText);
-      a.href = '#';
-      td.textContent = secondary;
-      td.append(a);
-      secondtr.appendChild(td);
-      table.appendChild(secondtr);
+      table.appendChild(tr);
+      secondaryKeys.forEach((secondary) => {
+        const secondtr = document.createElement('tr');
+        const td = document.createElement('td');
+        const a = document.createElement('a');
+        a.classList.add('square_btn35');
+        const linkText = document.createTextNode('x');
+        a.appendChild(linkText);
+        a.href = '#';
+        td.textContent = secondary;
+        td.append(a);
+        secondtr.appendChild(td);
+        table.appendChild(secondtr);
+      });
+
+      div.appendChild(table);
     });
-
-    div.appendChild(table);
-  });
-  const commands = document.querySelector('#commands');
-  commands.appendChild(div);
-  //   document.getElementById('commands').innerHTML = list;
+    const commands = document.querySelector('#commands');
+    commands.appendChild(div);
+  }
 });
